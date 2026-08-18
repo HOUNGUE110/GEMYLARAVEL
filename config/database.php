24ef6fa4735af133e56c1d38bf476d0053660e12
HOUNGUE110 <adigedeon6@gmail.com>
2026-08-18 09:25:22 +0000

    2 e essai

--- config/database.php
+++ config/database.php
@@ -18,7 +18,7 @@ return [
     */
 
     //'default' => env('DB_CONNECTION', 'sqlite'),
-    'default' => 'pgsql',
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
